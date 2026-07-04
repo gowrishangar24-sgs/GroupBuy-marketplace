0b4aa5bf26e14c04660f09a6a05223c08bb1eaae
gowrishangar24-sgs <gowrishangar24@gmail.com>
2026-07-04 16:24:21 +0530

    fix: match exact casing for Footer import in AddProduct page

--- my-react-app/src/pages/AddProduct.jsx
+++ my-react-app/src/pages/AddProduct.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer"; 
+import Footer from "../components/Footer"; 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
